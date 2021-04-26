89d17e70bfaeb2dd7d84ba3cae1ed39404020d6b
Con-imedia <emehado@gmail.com>
2021-04-26 23:09:13 +0000

    updated church fields and added crud operations for churches

--- controllers/events.controller.js
+++ controllers/events.controller.js
@@ -4,6 +4,16 @@ const Events = events(sequelize, Sequelize);
 const db = require("../models");
 const dateFns = require("date-fns");
 const _ = require("lodash");
+const multer = require("multer");
+const Op = Sequelize.Op;
+const debug = require("debug")("corner-stone:events-controller");
+//utils
+const { allowImagesOnly, storage } = require("../utils/image_upload");
+const upload = multer({
+  storage,
+  limits: { fileSize: 1024 * 1024 * 5 },
+  fileFilter: allowImagesOnly,
+}).single("church-image");
 
 const Controller = {};
 module.exports = Controller;
@@ -11,7 +21,9 @@ module.exports = Controller;
 Controller.getUpcomingEvents = async (req, res) => {
   const { churchId } = req.user;
   //get the users church adn get upcoming events for that church
-  const events = await Events.findAll({ where: { churchId } });
+  const events = await Events.findAll({
+    where: { churchId, eventDate: { [Op.gte]: new Date() } },
+  });
 
   events.forEach(
     (event) =>
@@ -21,16 +33,55 @@ Controller.getUpcomingEvents = async (req, res) => {
   res.json({ status: "00", data: events }); ///data is array of events
 };
 
-Controller.addEvents = async (req, res) => {
-  await Events.create({
-    eventVenue: "Accra",
-    imageBanner: "church-image-1618946666313.jpg",
-    eventDate: "2021-04-27",
-    eventTime: "09:30",
-    churchId: 2,
-    title: "Excursion to Mountains",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+Controller.addEvent = async (req, res) => {
+  //TODO - Get church id from session and insert into create
+  const page = "events/add";
+  await upload(req, res, async (err) => {
+    const { title, eventDate, eventTime } = req.body;
+    if (err) {
+      req.flash("error", err);
+      return res.render(page, { title: "Add Event", values: req.body });
+    }
+    const { error } = Events.validateEvent(req.body);
+    if (error) {
+      req.flash("error", error.details[0].message);
+      return res.render(page, { title: "Add Event", values: req.body });
+    }
+    //todo - query with church id
+    const eventExists = await Events.findOne({
+      where: { title, eventDate, eventTime },
+    });
+    if (eventExists) {
+      req.flash("error", "Event already exist");
+      return res.render(page, { title: "Add Event", values: req.body });
+    }
+    //todo - add church id
+    await Events.create({ ...req.body, image: req.file.filename });
+    req.flash("success", "Event added successfully");
+    return res.render(page, { title: "Events" });
   });
+
+  await Events.create();
   res.send("ok");
+};
+
+Controller.editEvent = async (req, res) => {
+  //TODO - Get church id from session and insert into create
+  const { id } = req.params;
+  const { error } = Events.validateEvent(req.body);
+  if (error) {
+    req.flash("error", error.details[0].message);
+    return res.render(page, { title: "Edit Event", values: req.body });
+  }
+  //todo - query with church id
+  const eventExists = await Events.findOne({
+    where: { id: { [Op.ne]: id }, title, eventDate, eventTime },
+  });
+  if (eventExists) {
+    req.flash("error", "Event already exist");
+    return res.render(page, { title: "Add Event", values: req.body });
+  }
+  await Events.create({ ...req.body, image: req.file.filename });
+  req.flash("success", "Event added successfully");
+  return res.render(page, { title: "Events" });
 };
