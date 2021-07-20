e55debfdfe2d87d10ce42f0c688cbe3f37b2e632
NanaSafo <brudexgh@gmail.com>
2021-07-20 23:40:32 +0000

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ cd ${PROJECT_FOLDER}
 echo "Bringing down with docker-compose down"
 docker-compose down
 echo "Bring up with docker-compose up"
-docker-compose up --build
+docker-compose up --build -d
