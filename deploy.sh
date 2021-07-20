6072ed17b352e52d0dad4a6dd9808c0707b4db7f
NanaSafo <brudexgh@gmail.com>
2021-07-20 23:37:57 +0000

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+PROJECT_FOLDER=/var/www/cornestone
+cd ${PROJECT_FOLDER}
 echo "Bringing down with docker-compose down"
 docker-compose down
-echo "Bring up with docker-compose up"300
+echo "Bring up with docker-compose up"
 docker-compose up --build
