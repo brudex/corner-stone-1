d9687794a5913a3e282a0366aa3c0d780a222a83
NanaSafo <brudexgh@gmail.com>
2021-07-22 17:58:15 +0000

    modified sendmail function to use axios'

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,13 +1,49 @@
 const nodemailer = require("nodemailer");
+const axios = require('axios');
+// const sendMail = async (options) => {
+//   const transporter = nodemailer.createTransport({
+//     host: "server283.web-hosting.com",
+//     auth: {
+//       user: "test@senyotheart.com",
+//       pass: "06k9uQfsAn",
+//     },
+//     secure: true,
+//   });
+//   await transporter.sendMail(options);
+// };
+
 const sendMail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: "server283.web-hosting.com",
-    auth: {
-      user: "test@senyotheart.com",
-      pass: "06k9uQfsAn",
-    },
-    secure: true,
+  const reqbody = {
+    "Messages":[
+      {
+        "From": {
+          "Email": "zenoteck.cornerstone@gmail.com",
+          "Name": "Cornerstone Giving"
+        },
+        "To": [
+          {
+            "Email": options.to,
+            "Name": "Cornerstone"
+          }
+        ],
+        "Subject": options.subject,
+        "TextPart": "Yes everyone.",
+        "HTMLPart": options.html,
+        "CustomID": "AppGettingStartedTest"
+      }
+    ]
+  };
+  axios.post('https://api.mailjet.com/v3.1/send', reqbody, {
+    headers: {
+      "Content-Type" : "application/json",
+      'Authorization' : 'Basic YWFhOWVmNDJmNmFiODJkZGNiMmJkYzViMzQ2MzYwMmQ6NmZkNWM4ZTdmZDlhZjFmNTM4ZmQ3ZjA0YTgzMTk0MWQ=',
+    }
+  }).then((response) => {
+    let responseBody = JSON.stringify(response.data);
+    console.log('Success mail sent:', responseBody);
+  }).catch((error) => {
+    console.error('Error response:', error);
   });
-  await transporter.sendMail(options);
+
 };
 module.exports = sendMail;
