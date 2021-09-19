89006ec2775b41a8e2e9e12b77e094a2dccfed0f
NanaSafo <brudexgh@gmail.com>
2021-09-19 02:07:31 +0000

    events fix

--- public/vendor/dom-factory.common.js
+++ public/vendor/dom-factory.common.js
@@ -223,7 +223,7 @@ const makeObservers = (src) => {
 /**
  * Get the configuration for DOM event listeners
  * @param  {Object} src The source object
- * @return {Array<Object(element, fn, church_events.js)>}
+ * @return {Array<Object(element, fn, events)>}
  */
 const listeners = (src) => {
   if (!isArray(src.listeners)) {
